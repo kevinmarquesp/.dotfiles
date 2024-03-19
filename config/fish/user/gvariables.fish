35efdc7fa7b27f3e0ed8bed7b45cc56d6f2246dd
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-03-19 14:30:19 -0300

    :children_crossing: ux (fish): disabled the .venv prompt indicator

--- config/fish/user/gvariables.fish
+++ config/fish/user/gvariables.fish
@@ -1,12 +1,12 @@
-set -gx BAT_THEME           'Nord'
+set -gx BAT_THEME "Nord"
 set -gx FZF_DEFAULT_COMMAND 'find . -type f -not -path "*/node_modules/*" -not -path "*/.git/*" | sed "s/^\.\///"'
-set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-
 set -gx EDITOR "vim"
+set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
+
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 
 set -gx PATH "/bin:/usr/bin:/usr/local/bin"
 set -gx PATH "$PATH:$HOME/.local/bin"
-
 set -gx PATH "$PATH:$PNPM_HOME"
 set -gx PATH "$PATH:$HOME/go/bin"
 set -gx PATH "$PATH:$HOME/.tmux/plugins/tmuxifier/bin"
