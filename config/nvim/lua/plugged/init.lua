a7cd414df2704cce82d139951ca89c0272a2e225
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-04-16 17:37:43 -0300

    :sparkles: feature (nvim): moved the plugins config modules to be called in init.lua file

--- config/nvim/lua/plugged/init.lua
+++ config/nvim/lua/plugged/init.lua
@@ -27,96 +27,34 @@ lazy.setup({
    "tpope/vim-surround",
    "andweeb/presence.nvim",
 
-   {
-      "shaunsingh/nord.nvim",
-      config = function()
-         require("plugged.config.nord")
-      end,
-   },
-   {
-      "nvim-lualine/lualine.nvim",
-      config = function()
-         require("plugged.config.lualine")
-      end,
-   },
-   {
-      "akinsho/bufferline.nvim",
-      dependencies = { "tiagovla/scope.nvim" },
-      config = function()
-         require("plugged.config.bufferline")
-      end,
-   },
-   {
-      "lewis6991/gitsigns.nvim",
-      config = function()
-         require("plugged.config.gitsigns")
-      end,
-   },
-   {
-      "lukas-reineke/indent-blankline.nvim",
-      config = function()
-         require("plugged.config.ibl")
-      end,
-   },
+   "shaunsingh/nord.nvim",
+   "nvim-lualine/lualine.nvim",
+   "lewis6991/gitsigns.nvim",
+   "lukas-reineke/indent-blankline.nvim",
+   "nvim-treesitter/nvim-treesitter",
+   "tpope/vim-commentary",
+   "windwp/nvim-autopairs",
 
-   {
-      "nvim-telescope/telescope.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      config = function()
-         require("plugged.config.telescope")
-      end,
-   },
-   {
-      "hrsh7th/nvim-cmp",
-      dependencies = {
-         "hrsh7th/cmp-nvim-lua",
-         "hrsh7th/cmp-nvim-lsp",
-         "hrsh7th/cmp-buffer",
-         "hrsh7th/cmp-path",
-         "hrsh7th/cmp-cmdline",
-         "saadparwaiz1/cmp_luasnip",
-      },
-      config = function()
-         require("plugged.config.cmp")
-      end,
-   },
-   {
-      "L3MON4D3/LuaSnip",
-      dependencies = { "rafamadriz/friendly-snippets" },
-      config = function()
-         require("plugged.config.luasnip")
-      end,
-   },
+   "tiagovla/scope.nvim",
+   "akinsho/bufferline.nvim",
 
-   {
-      "nvim-treesitter/nvim-treesitter",
-      config = function()
-         require("plugged.config.treesitter")
-      end,
-   },
-   {
-      "tpope/vim-commentary",
-      config = function()
-         require("plugged.config.commentary")
-      end,
-   },
-   {
-      "windwp/nvim-autopairs",
-      config = function()
-         require("plugged.config.autopairs")
-      end,
-   },
+   "rafamadriz/friendly-snippets",
+   "L3MON4D3/LuaSnip",
 
-   {
-      "williamboman/mason.nvim",
-      dependencies = {
-         "neovim/nvim-lspconfig",
-         "williamboman/mason-lspconfig.nvim",
-         "folke/neodev.nvim",
-         "j-hui/fidget.nvim",
-      },
-      config = function()
-         require("plugged.config.lspconfig")
-      end,
-   },
+   "nvim-lua/plenary.nvim",
+   "nvim-telescope/telescope.nvim",
+
+   "hrsh7th/cmp-nvim-lua",
+   "hrsh7th/cmp-nvim-lsp",
+   "hrsh7th/cmp-buffer",
+   "hrsh7th/cmp-path",
+   "hrsh7th/cmp-cmdline",
+   "saadparwaiz1/cmp_luasnip",
+   "hrsh7th/nvim-cmp",
+
+   "neovim/nvim-lspconfig",
+   "williamboman/mason-lspconfig.nvim",
+   "folke/neodev.nvim",
+   "j-hui/fidget.nvim",
+   "williamboman/mason.nvim",
 })
