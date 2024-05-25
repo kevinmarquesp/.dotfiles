c2a2c957604f44c5f10d2a288aba3a44aeba49b1
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-05-25 13:26:03 -0300

    :sparkles: feature (nvim): created an autocmd to start the lsp right when the buffer is open automatically

--- config/nvim/lua/user/autocmd.lua
+++ config/nvim/lua/user/autocmd.lua
@@ -1,9 +1,19 @@
--- Will use the Neovim LSP capabillyties to format the file before saving.
+-- Will use the Neovim LSP capabilities to format the file before saving.
 
 vim.api.nvim_create_autocmd("BufWrite", {
    pattern = "*",
 
    callback = function(_)
       vim.lsp.buf.format()
-   end
+   end,
+})
+
+-- Start the correct LSP automatically when enter because... Gotta go fast!
+
+vim.api.nvim_create_autocmd("BufEnter", {
+   pattern = "*",
+
+   callback = function(_)
+      vim.cmd("LspStart")
+   end,
 })
