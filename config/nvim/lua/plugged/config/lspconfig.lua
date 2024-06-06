e68113875b1d58522c4f42544ab5d90661a6a885
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-06 14:19:19 -0300

    :recycle: refactoring (nvim): now the excluded from format files is being configured on the lsp settings

--- config/nvim/lua/plugged/config/lspconfig.lua
+++ config/nvim/lua/plugged/config/lspconfig.lua
@@ -90,8 +90,16 @@ local servers = {
    },
    emmet_ls = {
       capabilities = capabilities,
-      filetypes = { "html", "templ", "javascript", "typescript", "react" },
+      filetypes = { "html", "templ", "javascript", "typescript", "typescriptreact", "javascriptreact" },
    },
+   html = {
+      on_attach = function(client, _)
+         client.server_capabilities.documentFormattingProvider = false
+         client.server_capabilities.documentRangeFormattingProvider = false
+      end,
+
+      capabilities = capabilities,
+   }
 }
 
 mason_lspconfig.setup({
--- config/nvim/lua/user/autocmd.lua
+++ config/nvim/lua/user/autocmd.lua
@@ -1,35 +1,9 @@
-function get_extension(filename)
-   local parts = {}
-
-   for w in filename:gmatch("([^.]+)") do
-      table.insert(parts, w)
-   end
-
-   return table.concat(parts, ".", 2)
-end
-
-function contains(table, element)
-   for _, value in pairs(table) do
-      if value == element then
-         return true
-      end
-   end
-
-   return false
-end
-
 -- Will use the Neovim LSP capabilities to format the file before saving.
 
 vim.api.nvim_create_autocmd("BufWrite", {
    pattern = "*",
 
    callback = function(event)
-      local extension = get_extension(event.file)
-
-      if contains({ "html", "edge" }, extension) then
-         return
-      end
-
       vim.lsp.buf.format()
    end,
 })
